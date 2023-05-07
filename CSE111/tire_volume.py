ac9fd6c58e9b2505ff9243e2692e832e822afeda
programmingcarpenter <ccarpenter2263@gmail.com>
2023-05-06 19:44:57 -0600

    Week 3 assignment

--- CSE111/tire_volume.py
+++ CSE111/tire_volume.py
@@ -1,6 +1,11 @@
 import math
+import datetime
+time_now = datetime.datetime.now()
+time_now = time_now.strftime('%Y-%m-%d')
 w = int(input('What is the Width of the tire (milimeters)? '))
 a = int(input('What is the Aspect Ratio of the tire? '))
 d = int(input('What is the Diameter of the tire (inches)? '))
 volume = ((math.pi * w**2 * a) * (w * a + 2540 * d)) / 10000000000
 print(f'The approximate volume of the tire is {volume:.2f} liters.')
+with open('volumes.txt', 'a') as text_file:
+    text_file.write(f'{time_now}, {w}, {a}, {d}, {volume:.2f}')
